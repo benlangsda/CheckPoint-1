12d4b545ac984f1fac84a9286d16bf4a4e4ac77a
msavoca <msavoca@ycp.edu>
2015-03-10 14:59:35 -0400

    view

--- CheckPoint/src/ycp/cs320/spring15/view/View.java
+++ CheckPoint/src/ycp/cs320/spring15/view/View.java
@@ -1,5 +1,21 @@
 package ycp.cs320.spring15.view;
 
-public class View {
+import java.util.Scanner;
 
+public class View {
+	public static void main(String [] args)
+	{
+		login();
+			
+	}
+	
+	public static void login() {
+		Scanner sc = new Scanner(System.in);
+		String user;
+		String pass;
+		System.out.println("Enter in your Username");
+		user = sc.nextLine();
+		System.out.println("Enter in your Password");
+		pass = sc.nextLine();
+	}
 }
